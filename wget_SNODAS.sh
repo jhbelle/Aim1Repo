9d1b30f11227991ffc2c6620df35ca3ff1690345
jhbelle <jessica.hartmann.belle@emory.edu>
2016-11-07 14:39:15 -0500

    Removed SNODAS download - using RUC/RAP estimates

--- wget_SNODAS.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-wget -nd -r --tries=45 "ftp://sidads.colorado.edu/DATASETS/NOAA/G02158/masked/200*"
-wget -nd -r --tries=45 "ftp://sidads.colorado.edu/DATASETS/NOAA/G02158/masked/201[0123]"
