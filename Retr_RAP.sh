aec4bb5f1b675e25c028dce3c2faae2fcc1337e7
jhbelle <jessica.hartmann.belle@emory.edu>
2016-12-21 12:50:57 -0500

    Set RUCRAP to run over holiday

---
+++ Retr_RAP.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+## ---------------------
+## Name: Retr_RUC.sh
+## Program version: grib_api; bash
+## Author: J.H. Belle
+## Purpose: Save data of interest from the RUC/RAP archives to text files for later processing
+## ---------------------
+
+source /home/jhbelle/.profile
+
+# Define variables
+var=( sp 10u 10v cape cin prate sd hpbl h h vis cape cin r )
+lev=( surface heightAboveGround heightAboveGround surface surface surface surface surface cloudBase cloudTop surface pressureFromGroundLayer pressureFromGroundLayer heightAboveGround )
+day=2007-04-01
+while [ "$day" != 2016-01-01 ]; do
+    #echo $day
+    year=$(date +%Y -d "$day")
+    month=$(date +%m -d "$day")
+    day2=$(date +%d -d "$day")
+        # Download RUC/RAP data for hours near or during passes over U.S. and write a text file for each variable and hour of each day (PRES:surface, UGRD:10 m, VGRD:10 m, CAPE:surface, CIN:surface,  PRATE:surface;, SNOD:surface, HPBL:surface, HGT:cloud base, HGT:cloud top, VIS:surface, CAPE:255-0 mb, CIN:255-0 mb)
+    filein=/terra/RUC_RAP_252/$year$month/$year$month$day2/rap_252_$year$month$day2
+    fileout=/gc_runs/RUCRAP_252_Intermediate/ruc2anl_252_$year$month$day2 
+    for ((i=0; i<=13; i++));
+    do  
+	for hour in 00 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23;
+	do
+	    filein+=_$hour
+	    filein+=00_000.grb2
+    	    fileout+=_$hour
+	    fileout+=_${var[$i]}_${lev[$i]}.txt
+            grib_get_data -m -9999 -p typeOfLevel,shortName -w shortName=${var[$i]},typeOfLevel=${lev[$i]} $filein>$fileout
+	    filein=/terra/RUC_RAP_252/$year$month/$year$month$day2/rap_252_$year$month$day2
+            fileout=/gc_runs/RUCRAP_252_Intermediate/ruc2anl_252_$year$month$day2 
+	done
+    done
+    day=$(date -I -d "$day + 1 day")
+done
+
+
