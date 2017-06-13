2769e42a562237aa254b18d43e5677af349b4eed
J.H. Belle <jessica.hartmann.belle@emory.edu>
2017-06-13 12:33:13 -0400

    Finished local linkage submits

--- LinkMODdat.R
+++ LinkMODdat.R
@@ -17,18 +17,18 @@ source("T://eohprojs/CDC_climatechange/Jess/Dissertation/Aim1Repo/Functions_Link
 #args = commandArgs(trailingOnly=T)
 #Endday = as.numeric(args[2])
 #Startday = as.numeric(args[1])
-Endday=366
-Startday=133
+Endday=365
+Startday=1
 ## Year
-Year = 2012
-TAflag="T"
+Year = 2013
+TAflag="A"
 ListBroken = c()
 ## Location of MODIS files - extracted from hdf section-specific csvs using GriddingExtractMODIS10km.m
 MODpath = "E://MODIScloud_extr/"
 ## Location of grid
 GridPath = "E://GriddedSF5x5/"
 ## Location of output files
-OutPath = "E://LinkedValsCloudCalifT/"
+OutPath = "E://LinkedValsCloudCalif/"
 ## Scale value for AOD - from MODIS hdf files
 Emisscale = 0.009999999776482582
 # --------
